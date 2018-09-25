8f1114e478d59f7bf66066e2c12754d97848836f
Eliot Easterling <eeasterl@kent.edu>
2018-09-25 14:56:58 -0400

    started adding custom middleware

--- CustomMiddleware/Startup.cs
+++ CustomMiddleware/Startup.cs
@@ -1,32 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace CustomMiddleware
+namespace Ui
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, 
+        // visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Information);
+            var logger = loggerFactory.CreateLogger("Middleware Demo");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                var timer = Stopwatch.StartNew();
+
+                // executes first
+                logger.LogInformation($"==> beginning request in {env.EnvironmentName}");
+
+                // executes next thing in pipeline
+                await next();
+
+                // executes after the pipeline has finished unwinding
+                logger.LogInformation($"==> request took {timer.ElapsedMilliseconds}ms");
+            });
+
+            app.Map("/Contacts", application =>
+            {
+                application.Run(async context =>
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("<h1>here are your contacts</h1>");
+
+                });
+            });
+
+            // serve static files from the wwwwroot folder
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
+                context.Response.ContentType = "text/html";
                 await context.Response.WriteAsync("Hello World!");
             });
         }
